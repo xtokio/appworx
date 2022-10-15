4266327dcf81fcfcb5c38e03944f6d1433703fee
Luis Gomez <xtokio@gmail.com>
2022-10-15 14:22:43 -0400

    Format schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,45 +1,45 @@
 BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS "users" (
-	"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	"username"			TEXT NOT NULL,
-	"password"			TEXT NOT NULL,
-	"name"	    		TEXT NOT NULL DEFAULT '',
-	"account_type"	TEXT NOT NULL,
-	"active"				INTEGER NOT NULL DEFAULT 1,
-	"created_at"		TEXT NOT NULL DEFAULT '',
-	"updated_at"		TEXT NOT NULL DEFAULT ''
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  "username"	  TEXT NOT NULL,
+  "password"	  TEXT NOT NULL,
+  "name"	  TEXT NOT NULL DEFAULT '',
+  "account_type"  TEXT NOT NULL,
+  "active"	  INTEGER NOT NULL DEFAULT 1,
+  "created_at"	  TEXT NOT NULL DEFAULT '',
+  "updated_at"	  TEXT NOT NULL DEFAULT ''
 );
 CREATE TABLE IF NOT EXISTS "jobs" (
-	"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  "user_id"				INTEGER NOT NULL DEFAULT 1,
-	"job"						TEXT NOT NULL DEFAULT '',
-	"description"		TEXT NOT NULL,
-	"schedule"			TEXT NOT NULL,
-	"schedule_time"	TEXT NOT NULL,
-	"active"				INTEGER NOT NULL DEFAULT 1,
-	"created_at"		TEXT NOT NULL DEFAULT '',
-	"updated_at"		TEXT NOT NULL DEFAULT ''
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  "user_id"	  INTEGER NOT NULL DEFAULT 1,
+  "job"		  TEXT NOT NULL DEFAULT '',
+  "description"	  TEXT NOT NULL,
+  "schedule"	  TEXT NOT NULL,
+  "schedule_time" TEXT NOT NULL,
+  "active"	  INTEGER NOT NULL DEFAULT 1,
+  "created_at"	  TEXT NOT NULL DEFAULT '',
+  "updated_at"	  TEXT NOT NULL DEFAULT ''
 );
 CREATE TABLE IF NOT EXISTS "tasks" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  "job_id"			INTEGER NOT NULL DEFAULT 1,
-	"task"				TEXT NOT NULL DEFAULT '',
-	"description"	TEXT NOT NULL DEFAULT '',
-	"language"		TEXT NOT NULL DEFAULT '',
-	"code"				TEXT NOT NULL DEFAULT '',
-	"active"			INTEGER NOT NULL DEFAULT 1,
-	"created_at"	TEXT NOT NULL DEFAULT '',
-	"updated_at"	TEXT NOT NULL DEFAULT ''
+  "job_id"	INTEGER NOT NULL DEFAULT 1,
+  "task"	TEXT NOT NULL DEFAULT '',
+  "description"	TEXT NOT NULL DEFAULT '',
+  "language"	TEXT NOT NULL DEFAULT '',
+  "code"	TEXT NOT NULL DEFAULT '',
+  "active"	INTEGER NOT NULL DEFAULT 1,
+  "created_at"	TEXT NOT NULL DEFAULT '',
+  "updated_at"	TEXT NOT NULL DEFAULT ''
 );
 CREATE TABLE IF NOT EXISTS "task_status" (
-	"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	"job_id"			INTEGER NOT NULL,
-	"task_id"			INTEGER NOT NULL,
-	"status"			INTEGER NOT NULL DEFAULT 0,
-	"response"		TEXT NOT NULL DEFAULT '',
-	"created_at"	TEXT NOT NULL DEFAULT '',
-	"updated_at"	TEXT NOT NULL DEFAULT ''
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  "job_id"	INTEGER NOT NULL,
+  "task_id"	INTEGER NOT NULL,
+  "status"	INTEGER NOT NULL DEFAULT 0,
+  "response"	TEXT NOT NULL DEFAULT '',
+  "created_at"	TEXT NOT NULL DEFAULT '',
+  "updated_at"	TEXT NOT NULL DEFAULT ''
 );
 
 COMMIT;
